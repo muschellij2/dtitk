32b6845b44618855a3c9899aedd8265f92f8bef2
muschellij2 <muschellij2@gmail.com>
2016-09-01 15:22:34 -0400

    Mixed up switch for download_itk

--- R/download_dtitk.R
+++ R/download_dtitk.R
@@ -9,8 +9,8 @@ download_dtitk = function(os = c("osx", "linux")) {
   os = match.arg(os)
   http_stub = "http://www.nitrc.org/frs/download.php"
   stub_url = switch(os,
-                    "osx" = "4981/dtitk-2.3.1-Linux-x86_64.tar.gz/?i_agree=1",
-                    "linux" = "5300/dtitk-2.3.3-Darwin-x86_64.tar.gz/?i_agree=1")
+                    "linux" = "4981/dtitk-2.3.1-Linux-x86_64.tar.gz/?i_agree=1",
+                    "osx" = "5300/dtitk-2.3.3-Darwin-x86_64.tar.gz/?i_agree=1")
   url = file.path(http_stub, stub_url)
   browseURL(url)
   return(invisible(NULL))
